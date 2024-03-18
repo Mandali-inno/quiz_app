692abeed0f895e672520c6052598177c400145e5
Mandali-inno <mandaliinno@gmail.com>
2024-03-18 19:56:28 +0200

    quiz to test

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/Admin/admin_login.dart';
 import 'package:quiz_app/pages/home.dart';
 import 'package:quiz_app/pages/question.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() async{
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "your_api_key",
+            authDomain: "your_auth_domain",
+            projectId: "your_project_id",
+            storageBucket: "your_storage_bucket",
+            messagingSenderId: "your_messaging_sender_id",
+            appId: "your_app_id",
+            measurementId: "your_measurement_id"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  try {
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: 'mandaliinno@gmail.com',
+      password: '123456',
+    );
+    debugPrint('Signed in successfully: ${userCredential.user!.uid}');
+  } catch (e) {
+    debugPrint('Sign in failed: $e');
+  }
+
   runApp(const MyApp());
 }
 
@@ -22,8 +48,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: AdminLogin(),
+      home: Home(),
     );
   }
 }
-
